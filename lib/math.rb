029e8986322583fc3bfc095c9a3fccc859259f6f
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-10-28 03:05:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,25 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+ difference = num1 - num2
+ return difference
 end
 
 def division(num1, num2)
-
+  division = num1/num2
+  return division
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  return product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  return remainder
 end
 
 def square_root(num)
-
+  
 end
